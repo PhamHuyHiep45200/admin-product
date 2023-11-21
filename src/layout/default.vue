d131fc80207bda51ff34a87a230930350ca9d3de
thang tomosia <hiep.pham@tomosia.com>
2023-11-21 21:54:06 +0700

    add base

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,61 +1,65 @@
 <template>
-  <a-layout>
-    <a-layout-header class="header">
+  <a-layout class="min-h-[100vh]">
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
       <div class="logo" />
-    </a-layout-header>
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <div v-for="key in [1,2]" :key="key">
-          {{ key }}
-        </div>
-      </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-        <a-layout-content
-          :style="{
-            background: '#fff',
-            padding: '24px',
-            margin: 0,
-            height: 'calc(100vh - 94px)',
-          }"
-        >
-          <slot />
-        </a-layout-content>
-      </a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff' }"
+        class="max-h-[calc(100vh-120px)] overflow-auto"
+      >
+        <slot/>
+      </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 <script>
-import {ref} from 'vue'
-
-const items = ref([
-  {
-    key: '1',
-    icon: '() => h(MailOutlined)',
-    label: 'Navigation One',
-    title: 'Navigation One',
-  },
-  {
-    key: '2',
-    icon: '() => h(CalendarOutlined)',
-    label: 'Navigation Two',
-    title: 'Navigation Two',
-  }])
+import { ref } from 'vue';
+const selectedKeys = ref(['1']);
+const collapsed = ref(false);
 </script>
-<style scoped>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
   background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
 }
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
+.site-layout .site-layout-background {
   background: #fff;
 }
 </style>
